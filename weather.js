d11f37e3b7f792b5d422f163ba89f7e6d8b3c2e7
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-21 21:15:23 -0500

    Adds the fetch function getWeather and changeLocation to our class

--- weather.js
+++ weather.js
@@ -1,7 +1,22 @@
 class Weather {
-  constructor(city, zipcode) {
+  constructor(city, zip) {
     this.apiKey = 'e27f465a672182232f7bb237e4eb9ea4';
     this.city = city;
-    this.zipcode = zipcode;
+    this.zip = zip;
+  }
+
+  // Fetch weather from API --> I used (https://openweathermap.org/api)
+  async getWeather() {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.zip}&APPID=${this.apiKey}`);
+  
+    const responseData = await response.json();
+  
+    return responseData;
+  }
+
+  // Change Location
+  changeLocation(city, zipCode) {
+    this.city = city;
+    this.zip = zip;
   }
 }
