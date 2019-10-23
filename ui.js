5f1a6b00680ec062e405ab8a8f11d4521bc4ddc1
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-22 19:59:17 -0500

    Adds an event listener that loads the getWeather function when the DOM loads

--- ui.js
+++ ui.js
@@ -1,8 +1,14 @@
 // Initialize weather object 
 const weather = new Weather('Chicago', 'us');
 
-weather.getWeather()
-  .then(results => {
-    console.log(results);
-  })
-  .catch(err => console.log(err));
+// Get wether on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+// weather.changeLocation('Miami', 'us');
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => console.log(err));
+}
