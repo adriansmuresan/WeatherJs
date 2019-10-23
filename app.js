3d620dbda862977a87b5665258913a1090ecea71
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-23 15:58:34 -0500

    Adds an event listener to the save button on the modal to get the city and country values and then call the appropriate functions to get the data

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', getWeather);
 // weather.changeLocation('Miami', 'us');
 
+// Change Location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  weather.changeLocation(city, country);
+
+  // Get and display weather
+  getWeather();
+
+  // Close modal
+  $('#locModal').modal('hide');
+
+})
+
 function getWeather() {
   weather.getWeather()
     .then(results => {
