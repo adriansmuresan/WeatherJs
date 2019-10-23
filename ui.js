b506e877453da072c0d0feefec6ed08d4804861b
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-22 21:25:03 -0500

    Adds in the params for the paint function

--- ui.js
+++ ui.js
@@ -5,8 +5,16 @@ class UI {
     this.string = document.getElementById('w-string');
     this.details = document.getElementById('w-details');
     this.humidity = document.getElementById('w-humidity');
-    this.dewpoint = document.getElementById('w-dewpoint');
-    this.feelsLIke = document.getElementById('w-feels-like');
+    this.pressure = document.getElementById('w-pressure');
     this.wind = document.getElementById('w-wind');
+  }
+
+  paint(weather) {
+    this.location.textContent = `${weather.name}`;
+    this.desc.textContent = `${weather.weather[0].description}`.toUpperCase();
+    this.string.textContent = `${weather.main.temp} F`;
+    this.humidity.textContent = `Humidity: ${weather.main.humidity}%`;
+    this.pressure.textContent = `Pressure: ${weather.main.pressure} hPa`;
+    this.wind.textContent = `Wind speed: ${weather.wind.speed} mph`;
   }
 }
