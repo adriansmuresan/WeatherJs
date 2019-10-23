cb196574f4666e82af0482d3a0df6e3cfb46712a
Adrian S MUresan <adriansmuresan@gmail.com>
2019-10-22 20:16:42 -0500

    Adds the UI constructor class

--- ui.js
+++ ui.js
@@ -1,14 +1,12 @@
-// Initialize weather object 
-const weather = new Weather('Chicago', 'us');
-
-// Get wether on DOM load
-document.addEventListener('DOMContentLoaded', getWeather);
-// weather.changeLocation('Miami', 'us');
-
-function getWeather() {
-  weather.getWeather()
-    .then(results => {
-      console.log(results);
-    })
-    .catch(err => console.log(err));
+class UI {
+  constructor() {
+    this.location = document.getElementById('w-location');
+    this.desc = document.getElementById('w-desc');
+    this.string = document.getElementById('w-string');
+    this.details = document.getElementById('w-details');
+    this.humidity = document.getElementById('w-humidity');
+    this.dewpoint = document.getElementById('w-dewpoint');
+    this.feelsLIke = document.getElementById('w-feels-like');
+    this.wind = document.getElementById('w-wind');
+  }
 }
